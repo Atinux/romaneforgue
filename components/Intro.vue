8c624e998d44e6548889e775f7e3294292ad9fef
Alexandre Chopin <wikeo65@gmail.com>
2017-02-20 12:00:07 +0100

    update typo

--- components/Intro.vue
+++ components/Intro.vue
@@ -12,7 +12,7 @@
             Après une licence en Langues Étrangères Appliquées (Anglais et Espagnol) je me suis reconvertie vers le <b>Webdesign</b><br/>
             depuis maintenant presque <b>2 ans</b>.<br/>
             Actuellement étudiante en année préparatoire au <b>master en Webdesign à Digital Campus</b> (Bordeaux),
-            je cherche un <b>stage</b> dans ce domaine à partir du <b>mois d'avril</b> pour une durée de <b>3 à 6 mois sur Bordeaux.</b><br/>
+            je cherche un <b>stage</b> dans ce domaine à partir du <b>mois d'avril</b> pour une durée de <b>3 à 6 mois à Bordeaux.</b><br/>
             Si mon profil vous intéresse, je vous invite à <b>télécharger mon CV</b> ainsi qu’à découvrir <b>mes créations</b>.
           </p>
         </section>
